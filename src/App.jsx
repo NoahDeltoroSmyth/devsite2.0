189f306619da0855494a6f20268db4af868fec85
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-14 18:14:01 -0700

    Resume linked, bio page skeleton set

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Noah Del Toro Smyth</h1>
+      {/* TODO:Create a logo for my brand */}
+      <img src='profilepic.jpg' alt='Noah Smyth'></img>
+      <h2>software engineer with a keen eye for design, accuracy, and a passion for problem-solving. Well-versed in the HTML, CSS, and Javascript languages.
+         At heart an artist of functionality welcoming to new challenges that strengthen the ability to write DRY code.</h2>
+      <h3>Contact Me!</h3>
+      <a href='https://www.canva.com/design/DAE4pvlZQA0/view' target="_blank" rel="noreferrer"><h3>Resume</h3></a>
+    </div>
+  );
+}
+
+export default App;
