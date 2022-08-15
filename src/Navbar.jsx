cc57d5cbcb9606fcade00888414f8c671fbc8c39
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-14 20:17:08 -0700

    dropdown state functionality working. Needs css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,13 +3,32 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
 
   return (
     <nav>
-      <Link to="/">Home</Link>
       <div onClick={handleClick}>
-        {click ? <img src="dropdownicon.png" alt="dropdown menu" /> : <img src="closeicon.png" alt="close menu" />}
+        {click ? (
+          <>
+            <img src="closeicon.png" alt="close menu" />
+            <ul onClick={handleClick}>
+              <Link to="/">
+                <li>About Me</li>
+              </Link>
+              <Link to="coding-projects">
+                <li>Coding Projects</li>
+              </Link>
+              <Link to="personal-projects">
+                <li>Personal Projects</li>
+              </Link>
+              <Link to="contact">
+                <li>Contact Me</li>
+              </Link>
+            </ul>
+          </>
+        ) : (
+          <img src="dropdownicon.png" alt="dropdown menu" />
+        )}
       </div>
     </nav>
   );
