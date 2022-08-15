789366f38565ac1ecec979163211e5927c7e1b4e
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-14 19:00:00 -0700

    Contact route working

---
+++ src/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+             <h1>Contact Me!</h1>
+      <a href="https://github.com/NoahDeltoroSmyth" target="_blank" rel="noreferrer">
+        <p>Github</p>
+      </a>
+      <a href="https://www.linkedin.com/in/noahdeltorosmyth/" target="_blank" rel="noreferrer">
+        <p>Linkedin</p>
+      </a>
+      <a href="mailto:noahsmyth96@gmail.com">
+        <p>Email: Noahsmyth96@gmail.com</p>
+      </a>
+      <a href="https://www.canva.com/design/DAE4pvlZQA0/view" target="_blank" rel="noreferrer">
+        <p>Resume</p>
+      </a>
+        </div>
+    );
+}
+
+export default Contact;
