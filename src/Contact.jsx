18eae9e4c80eec77b2cb60e23e2f0dc531f0bcf7
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-17 14:24:41 -0700

    added captions to contact images

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,27 +7,34 @@ function Contact() {
       <hr></hr>
       <div className="contact-links">
         <a
+          className="link-anchor"
           href="https://www.linkedin.com/in/noahdeltorosmyth/"
           target="_blank"
           rel="noreferrer"
         >
+          <p className="link-caption">Linkedin</p>
           <img className="link-image" src="linkedin.gif" alt="linkedin logo" />
         </a>
-        <a href="mailto:noahsmyth96@gmail.com">
+        <a className="link-anchor" href="mailto:noahsmyth96@gmail.com">
+          <p className="link-caption">Email</p>
           <img className="link-image" src="email4.gif" alt="email logo" />
         </a>
         <a
+          className="link-anchor"
           href="https://www.canva.com/design/DAE4pvlZQA0/view"
           target="_blank"
           rel="noreferrer"
         >
+          <p className="link-caption">Resume</p>
           <img className="link-image" src="resume.gif" alt="resume logo" />
         </a>
         <a
+          className="link-anchor"
           href="https://www.instagram.com/_metallurgia_/"
           target="_blank"
           rel="noreferrer"
         >
+          <p className="link-caption">Instagram</p>
           <img
             className="link-image"
             src="instagram.gif"
@@ -35,10 +42,12 @@ function Contact() {
           />
         </a>
         <a
+          className="link-anchor"
           href="https://github.com/NoahDeltoroSmyth"
           target="_blank"
           rel="noreferrer"
         >
+          <p className="link-caption">Github</p>
           <img className="link-image" src="github.gif" alt="github logo" />
         </a>
       </div>
