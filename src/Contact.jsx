57479c6975f13be53eacec20a9534835271918d3
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-17 11:15:26 -0700

    contact me page animated and css'd

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React from "react";
 
 function Contact() {
-    return (
-        <div>
-             <h1>Contact Me!</h1>
-      <a href="https://github.com/NoahDeltoroSmyth" target="_blank" rel="noreferrer">
-        <p>Github</p>
-      </a>
-      <a href="https://www.linkedin.com/in/noahdeltorosmyth/" target="_blank" rel="noreferrer">
-        <p>Linkedin</p>
-      </a>
-      <a href="mailto:noahsmyth96@gmail.com">
-        <p>Email: Noahsmyth96@gmail.com</p>
-      </a>
-      <a href="https://www.canva.com/design/DAE4pvlZQA0/view" target="_blank" rel="noreferrer">
-        <p>Resume</p>
-      </a>
-        </div>
-    );
+  return (
+    <>
+      {/* <h1 className="contact-header">
+        Get a hold of me at any of the links below
+      </h1> */}
+      <hr></hr>
+      <div className="contact-links">
+        <a
+          href="https://github.com/NoahDeltoroSmyth"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Github</p>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/noahdeltorosmyth/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Linkedin</p>
+        </a>
+        <a href="mailto:noahsmyth96@gmail.com">
+          <p>Noahsmyth96@gmail.com</p>
+        </a>
+        <a
+          href="https://www.canva.com/design/DAE4pvlZQA0/view"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Resume</p>
+        </a>
+        <a
+          href="https://www.instagram.com/_metallurgia_/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Instagram</p>
+        </a>
+      </div>
+      <hr></hr>
+    </>
+  );
 }
 
 export default Contact;
