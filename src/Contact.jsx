641579e73b7a667b55c9a82cee62445eeb32f37a
NoahDeltoroSmyth <noahsmyth96@gmail.com>
2022-08-17 13:15:41 -0700

    added gifs for contact me page. contact page css near complete for now

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,41 +3,43 @@ import React from "react";
 function Contact() {
   return (
     <>
-      {/* <h1 className="contact-header">
-        Get a hold of me at any of the links below
-      </h1> */}
+      <h1 className="contact-header">Get in Touch!</h1>
       <hr></hr>
       <div className="contact-links">
-        <a
-          href="https://github.com/NoahDeltoroSmyth"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p>Github</p>
-        </a>
         <a
           href="https://www.linkedin.com/in/noahdeltorosmyth/"
           target="_blank"
           rel="noreferrer"
         >
-          <p>Linkedin</p>
+          <img className="link-image" src="linkedin.gif" alt="linkedin logo" />
         </a>
         <a href="mailto:noahsmyth96@gmail.com">
-          <p>Noahsmyth96@gmail.com</p>
+          <img className="link-image" src="email4.gif" alt="email logo" />
         </a>
         <a
           href="https://www.canva.com/design/DAE4pvlZQA0/view"
           target="_blank"
           rel="noreferrer"
         >
-          <p>Resume</p>
+          <img className="link-image" src="resume.gif" alt="resume logo" />
         </a>
         <a
           href="https://www.instagram.com/_metallurgia_/"
           target="_blank"
           rel="noreferrer"
         >
-          <p>Instagram</p>
+          <img
+            className="link-image"
+            src="instagram.gif"
+            alt="instagram logo"
+          />
+        </a>
+        <a
+          href="https://github.com/NoahDeltoroSmyth"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="link-image" src="github.gif" alt="github logo" />
         </a>
       </div>
       <hr></hr>
